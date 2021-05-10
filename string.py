0f395854cffc8104e418c7c824b03ecf0c721f09
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-05-10 09:30:09 +0300

    Update string.py

--- string.py
+++ string.py
@@ -75,3 +75,22 @@ def password_generator(user_name):
     return password
   
   
+#10 Using .split() and the provided string, create a list called author_names containing each individual author name as it’s own string.
+
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+#11 Create another list called author_last_names that only contains the last names of the poets in the provided string.
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
+
+
+
+
