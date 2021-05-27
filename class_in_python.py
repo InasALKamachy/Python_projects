ba4d78bf09aad84cb86f33364b1fb957836fff23
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-05-27 11:52:42 +0300

    Update class_in_python.py

--- class_in_python.py
+++ class_in_python.py
@@ -1,5 +1,17 @@
 class DistanceConverter:
   kms_in_a_mile = 1.609
+  def how_many_kms(self, miles): # this is method
+    return miles * self.kms_in_a_mile
+ 
+converter = DistanceConverter() # define the name of class
+kms_in_5_miles = converter.how_many_kms(5)
+print(kms_in_5_miles)
+# prints "8.045"
+
+
+
+class DistanceConverter:
+  kms_in_a_mile = 1.609
   def how_many_kms(self, miles):
     return miles * self.kms_in_a_mile
  
